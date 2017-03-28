4c45994d403fe4b4493aff72b20cf7c3a593e5c8
Sahil Makhijani <svmakhijani94@gmail.com>
2017-03-28 11:41:00 -0400

    I think I fixed what isn't working

--- gradient_descent.py
+++ gradient_descent.py
@@ -13,25 +13,25 @@ def main():
     w0 = 0.1
     w1 = 0.2
     a = 0.5
-    # max = 1000000
     batch(w0, w1, a, dataset)
     stochastic(w0, w1, a, dataset)
 
 
-def batch(t0, t1, a, data):
-    t01 = t0
-    t11 = t1
+def batch(w0, w1, a, data):
+    old0 = w0
+    old1 = w1
     count = 0
-    while count < 3:
-        print("t0: " + str(t0) + ", t01: " + str(t01))
-        t01 = t01 + a * (summation(t01, t11, 1, data, 0))
-        t11 = t11 + a * (summation(t01, t11, 1, data, 1))
+    while count < 1000000:
+        new0 = old0 + a * (summation(old0, old1, 1, data, 0))
+        new1 = old1 + a * (summation(old0, old1, 1, data, 1))
+        old0 = new0
+        old1 = new1
         count += 1
-        print("t0: " + str(t01) + ", t1: " + str(t11) + ", " + str(count))
-        if (abs(t0 - t01) < 10**-10) and (abs(t1 - t11) < 10**-10):
-            print("Weight 1: " + str(t01) + "\nWeight 2: " + str(t11))
+        # print("t0: " + str(new0) + ", t1: " + str(new1) + ", " + str(count))
+        if (abs(w0 - new0) < 10**(-10)) and (abs(w1 - new1) < 10**(-10)):
+            print("Weight 1: " + str(new0) + "\nWeight 2: " + str(new1))
             break
-        if math.isinf(t01) or math.isinf(t11) or math.isnan(t01) or math.isnan(t11):
+        if math.isinf(new0) or math.isinf(new1) or math.isnan(new0) or math.isnan(new1):
             print("Breaks at epoch " + str(count))
             break
 
@@ -40,8 +40,8 @@ def stochastic(t0, t1, a, data):
     t01 = t0
     t11 = t1
     count = 0
+    # max = 1000000
     while count < 1000000:
-        # print("t0: " + str(t01) + ", t1: " + str(t11) + ", " + str(count))
         t01 = summation(t01, t11, a, data, 0)
         t11 = summation(t11, t11, a, data, 1)
         count += 1
